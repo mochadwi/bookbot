d1cbc2d2dfb9e50ad96f058fe23b1975132f9826
mochadwi <iqbal@bwc.ae>
2024-12-28 10:30:46 +0700

    count unique letter on each word

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ with open("books/frankenstein.txt", "r") as f:
     characters = file_contents.split()
 
     print(len(characters))
+
+    characters_dict = {}
+    for word in characters:
+        for char in word:
+            letter = char.lower()
+            if letter in characters_dict:
+                characters_dict[letter] += 1
+            else:
+                characters_dict[letter] = 1
+    
+    print(characters_dict)
