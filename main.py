15eceaa87ea3c03ceb31c75842861fb7690ebfd9
mochadwi <iqbal@bwc.ae>
2024-12-28 10:39:12 +0700

    reports for counted unique letters only

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 with open("books/frankenstein.txt", "r") as f:
     file_contents = f.read()
-    print(file_contents)
+    # print(file_contents)
 
     characters = file_contents.split()
 
-    print(len(characters))
+    # print(len(characters))
 
     characters_dict = {}
     for word in characters:
@@ -15,4 +15,11 @@ with open("books/frankenstein.txt", "r") as f:
             else:
                 characters_dict[letter] = 1
     
-    print(characters_dict)
+    # print(characters_dict)
+    print("--- Begin report of {f.name} ---")
+    print(f"{len(characters)} words found in the document")
+    for char in sorted(characters_dict, key=characters_dict.get, reverse=True):
+        if char.isalpha():
+            print(f"The '{char}' character was found {characters_dict[char]} times")
+
+    print("--- End report ---")
